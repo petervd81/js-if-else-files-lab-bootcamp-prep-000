d6dd2f86a656367569e883007ba1ecf2edc25542
Peter van Dijk <peter@petervandijk.net>
2018-05-19 09:18:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var name = "Joe";
-var height = "74";
+var name = "Joe"
+var height = "74"
 
-if (name === "susan") {
-  console.log("the name is correct");
-}
 
 // Don't worry about this
-module.exports = { name, height };
+module.exports = { name, height
+}
 
